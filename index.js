113b4214619e7412d1d9d7a24d3d0ce21f37bac8
Gustavo <gustavoaraujo086@gmail.com>
2026-03-21 17:19:06 -0300

    feat: add node app inital config

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello API!');
+});
+
+app.listen(process.env.APP_PORT);
